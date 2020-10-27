6437648d3d223002f890452a545d4d997a667b18
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 12:16:54 +0530

    added code for calculating wages for a month

--- employee_Wage_Computation.sh
+++ employee_Wage_Computation.sh
@@ -1,17 +1,24 @@
+#!/bin/bash -x
 isparttime=1;
 isfulltime=2;
+totalsalary=0;
 emprateperhr=20;
-empcheck=$((RANDOM%3));
-case $empcheck in
-	$isfulltime)
-	emphrs=8
-	;;
-	$isparttime)
-	emphrs=4
-	;;
-	*)
-	emphrs=0
-	;;
-esac
-salary=$(($emphrs*$emprateperhr));
-echo $salary
+numworkingdays=20;
+for (( day=1; day<=$numworkingdays; day++ ))
+do
+	empcheck=$((RANDOM%3));
+		case $empcheck in
+			$isfulltime)
+				emphrs=8
+				;;
+			$isparttime)
+				emphrs=4
+				;;
+			*)
+			emphrs=0
+				;;
+		esac
+		salary=$(($emphrs*$emprateperhr));
+		totalsalary=$(($totalSalary+$salary));
+done
+echo $totalsalary
